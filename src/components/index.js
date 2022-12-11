0bad6410b9622faa9278dfb34d7d784633d3f9ca
Ajeet Singh <2016appsingh@gmail.com>
2022-12-11 21:48:50 +0530

    Changes

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Features from './Features';
 import Download from './Download';
 import SectionWrapper from './SectionWrapper';
 import Navbar from './Navbar';
+import UnderConstruction from './UnderConstruction';
 
-export { Features, Download, SectionWrapper, Navbar };
+export { Features, Download, SectionWrapper, Navbar, UnderConstruction };
